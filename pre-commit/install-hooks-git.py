1d4815c3435911d5fd797bdff9030608c7ce39bc
Chris Hall <explosivegnome@yahoo.com>
2026-01-28 21:43:06 +0800

    working on windows support

---
+++ pre-commit/install-hooks-git.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import os, sys, platform, re
+from pathlib import Path
+
+def err(*args, **wkargs):
+	print(*args, **wkargs, file=sys.stderr)
+
+# first check that this is a git repo
+this_dir = Path(__file__).parent
+root_dir = this_dir.parent
+git_dir = root_dir / ".git"
+if not git_dir.is_dir():
+	err(f"ERROR: Directory '{str(git_dir)} does not exist'")
+	exit(1)
+
+# figure out which versions of scripts to use
+if platform.system() == "Windows":
+	suffix = ".PS1"
+	prefix = "powershell.exe -File "
+else:
+	suffix = ".sh"
+	prefix = "bash "
+
+# read the pre-commit file, if it exists
+git_precommit_file = git_dir / "hooks" / "pre-commit"
+if git_precommit_file.exists():
+	git_precommit_content = git_precommit_file.read_text()
+else:
+	git_precommit_content = "#!/bin/sh\n\n"
+
+# find pre-commit hook scripts and add them
+err(f"Installing pre-commit hook scripts to '{str(git_precommit_file)}'...")
+changed = False
+for p in this_dir.rglob('*'):
+	if p.name.endswith(suffix):
+		hook_entry = f'{prefix}{str(p.relative_to(root_dir)).replace('\\','/')}'
+		if hook_entry not in git_precommit_content:
+			err(f"installing hook '{hook_entry}'")
+			git_precommit_content += f'echo "{p.stem}..."\n' + hook_entry+'\n'
+			changed = True
+		else:
+			err(f"hook '{hook_entry}' already installed")
+if changed:
+	git_precommit_file.write_text(git_precommit_content)
+err("...Done")
+exit(0)
+
--- pre-commit/precommit.rustfmt.sh
+++ pre-commit/precommit.rustfmt.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+# NOTE: runs in repo root
 
 set -eo pipefail
 
-cd "$(dirname "$0")/.."
-RUST_FILES="$($(hg status -man 2>/dev/null || git diff --name-only --staged) | egrep "\.rs$")"
+RUST_FILES="$(echo $(hg status -man 2>/dev/null || git diff --name-only --staged) | egrep "\.rs$")"
 if [ -z $RUST_FILES ]; then
 	echo "Skipping rustfmt"
 else
-	echo "Formatting rust files with rustfmt: $(echo $RUST_FILES)"
-	rustfmt $RUST_FILES
+	echo "Formatting rust files with rustfmt: $RUST_FILES"
+	set +e
+	rustfmt --check $RUST_FILES
+	EXIT_CODE=$?
+	set -e
+	if [ $EXIT_CODE -ne 0 ]; then
+		rustfmt $RUST_FILES
+		echo "Reformatting changes applied. Re-commit to keep changes."
+		exit 1
+	fi
 fi
