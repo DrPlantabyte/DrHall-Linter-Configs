3fc38074eba32ae6e6d933e78a6bb0546f3916f0
Chris Hall <explosivegnome@yahoo.com>
2026-01-30 16:49:45 +0800

    works on linux too

--- pre-commit/install-hooks-git.py
+++ pre-commit/install-hooks-git.py
@@ -17,9 +17,11 @@ if not git_dir.is_dir():
 if platform.system() == "Windows":
 	suffix = ".PS1"
 	prefix = "powershell.exe -File "
+	is_windows = True
 else:
 	suffix = ".sh"
 	prefix = "bash "
+	is_windows = False
 
 # read the pre-commit file, if it exists
 git_precommit_file = git_dir / "hooks" / "pre-commit"
@@ -33,7 +35,8 @@ err(f"Installing pre-commit hook scripts to '{str(git_precommit_file)}'...")
 changed = False
 for p in this_dir.rglob('*'):
 	if p.name.endswith(suffix):
-		hook_entry = f'{prefix}{str(p.relative_to(root_dir)).replace('\\','/')}'
+		hook_cmd = str(p.relative_to(root_dir)).replace("\\","/")
+		hook_entry = f'{prefix}{hook_cmd}'
 		if hook_entry not in git_precommit_content:
 			err(f"installing hook '{hook_entry}'")
 			git_precommit_content += f'echo "{p.stem}..."\n' + hook_entry+'\n'
@@ -42,6 +45,8 @@ for p in this_dir.rglob('*'):
 			err(f"hook '{hook_entry}' already installed")
 if changed:
 	git_precommit_file.write_text(git_precommit_content)
+	if not is_windows:
+		os.chmod(git_precommit_file, 0o777)
 err("...Done")
 exit(0)
 
