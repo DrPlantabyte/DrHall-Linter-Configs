83840b38da1fbd71f5fd854530fecbc80ec95366
Chris Hall <explosivegnome@yahoo.com>
2026-01-30 16:58:42 +0800

    Pre-commit install scripts for hg and git (#1)
    * added rustfmt.toml
    
    * Git and Mercurial hooks working on Windows, need to re-test on Linux
    
    * works on linux too

---
+++ pre-commit/install-hooks-hg.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import os, sys, platform, re
+from pathlib import Path
+
+def err(*args, **wkargs):
+	print(*args, **wkargs, file=sys.stderr)
+
+# first check that this is a mercurial repo
+this_dir = Path(__file__).parent
+root_dir = this_dir.parent
+hg_dir = root_dir / ".hg"
+if not hg_dir.is_dir():
+	err(f"ERROR: Directory '{str(hg_dir)} does not exist'")
+	exit(1)
+
+# read the hgrc file, if it exists
+hgrc_file = hg_dir / "hgrc"
+if hgrc_file.exists():
+	hgrc_content = hgrc_file.read_text()
+else:
+	hgrc_content = ''
+
+# find the [hooks] section, if it exists
+hook_matches = list(re.finditer(r'^\w*\[\w*hooks\w*\]', hgrc_content))
+if len(hook_matches) > 0:
+	insert_index = hook_matches[-1].end(0)
+	# move to index after next newline
+	insert_index = hgrc_content.find('\n', insert_index)
+	if insert_index == -1:
+		# there is no new-line after [hooks]
+		hgrc_content += '\n'
+		insert_index = len(hgrc_content)
+else:
+	hgrc_content = hgrc_content + "\n[hooks]\n"
+	insert_index = len(hgrc_content)
+
+# figure out which versions of scripts to use
+if platform.system() == "Windows":
+	suffix = ".PS1"
+	prefix = "powershell.exe -File "
+else:
+	suffix = ".sh"
+	prefix = "bash "
+
+# find pre-commit hook scripts and add them
+err(f"Installing pre-commit hook scripts to '{str(hgrc_file)}'...")
+changed = False
+for p in this_dir.rglob('*'):
+	if p.name.endswith(suffix):
+		hook_entry = f'{p.stem} = {prefix}{str(p.relative_to(root_dir))}'
+		if hook_entry not in hgrc_content:
+			err(f"installing hook '{hook_entry}'")
+			hgrc_content = hgrc_content[:insert_index]+hook_entry+'\n'+hgrc_content[insert_index:]
+			insert_index += 1+len(hook_entry)
+			changed = True
+		else:
+			err(f"hook '{hook_entry}' already installed")
+if changed:
+	hgrc_file.write_text(hgrc_content)
+err("...Done")
+exit(0)
+
---
+++ pre-commit/precommit.rustfmt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# NOTE: runs in repo root
+
+cd "$(dirname "$0")/.."
+
+RUST_FILES="$(echo $(hg status -man 2>/dev/null || git diff --name-only --staged) | egrep "\.rs$")"
+if [ -z $RUST_FILES ]; then
+	echo "Skipping rustfmt"
+else
+	echo "Formatting rust files with rustfmt: $RUST_FILES"
+	rustfmt --check $RUST_FILES
+	EXIT_CODE=$?
+	set -e
+	if [ $EXIT_CODE -ne 0 ]; then
+		rustfmt $RUST_FILES
+		echo "Reformatting changes applied. Re-commit to keep changes."
+		exit 1
+	fi
+fi
