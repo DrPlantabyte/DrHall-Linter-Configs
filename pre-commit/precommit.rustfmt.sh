9bdde720d93ce83496251dcb8b7099f3cca8f506
Chris Hall <explosivegnome@yahoo.com>
2026-01-30 16:53:25 +0800

    wip

--- pre-commit/precommit.rustfmt.sh
+++ pre-commit/precommit.rustfmt.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # NOTE: runs in repo root
 
-set -eo pipefail
 cd "$(dirname "$0")/.."
-
+echo "collecting rust files"
 RUST_FILES="$(echo $(hg status -man 2>/dev/null || git diff --name-only --staged) | egrep "\.rs$")"
+echo "$?    rust files: $RUST_FILES"
 if [ -z $RUST_FILES ]; then
 	echo "Skipping rustfmt"
 else
 	echo "Formatting rust files with rustfmt: $RUST_FILES"
-	set +e
 	rustfmt --check $RUST_FILES
 	EXIT_CODE=$?
 	set -e
